7101bfac20ed2eb161cf8cbc1775bba92cad3d88
selim <selim.ozturk@afiniti.com>
2021-11-19 16:46:34 +0300

    red://

--- blackops/taskq/redis.py
+++ blackops/taskq/redis.py
@@ -1,9 +1,9 @@
 import aioredis
 import redis
 
-redis_url = "redis:6379"
+redis_url = "redis://redis:6379"
 
 # single instance
 aio_redis_client: aioredis.Redis = aioredis.from_url(redis_url)
 
-redis_client = redis.Redis(host="redis", port=6379)
+redis_client = redis.Redis(host="redis://redis", port=6379)
