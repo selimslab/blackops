034351b8529d728c31dffcb629b443adb8a0bcf7
selim <selim.ozturk@afiniti.com>
2021-11-19 16:56:19 +0300

    add COMPOSE_TLS_VERSION

--- Dockerfile
+++ Dockerfile
@@ -23,5 +23,7 @@ WORKDIR /blackops
 EXPOSE 7846
 EXPOSE 5555
 
+ENV COMPOSE_TLS_VERSION TLSv1_2
+
 ENTRYPOINT [ "bash","/blackops/docker-entrypoint.sh" ]
 CMD ["python", "-m", "uvicorn", "blackops.api.main:app", "--host", "0.0.0.0", "--port", "7846", "--workers", "2"]
--- blackops/taskq/redis.py
+++ blackops/taskq/redis.py
@@ -4,6 +4,6 @@ import redis
 redis_url = "redis://redis:6379"
 
 # single instance
-aio_redis_client: aioredis.Redis = aioredis.from_url(redis_url)
+# aio_redis_client: aioredis.Redis = aioredis.from_url(redis_url)
 
 redis_client = redis.Redis(host="redis://redis", port=6379)
