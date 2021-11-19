be91a6c7c176e94e68329f988dc90a6dd689cf79
selim <selim.ozturk@afiniti.com>
2021-11-19 19:56:22 +0300

    up

--- blackops/taskq/redis.py
+++ blackops/taskq/redis.py
@@ -1,15 +1,16 @@
 import os
 
-import aioredis
 import redis  # type: ignore
 
 from blackops.util.logger import logger
 
-redis_host = os.getenv("REDIS_HOST", "NO REDIS")
+redis_host = os.environ["REDIS_HOST"]
 
-logger.info(f"redis_hostl: {redis_host}")
+logger.info(f"redis_host: {redis_host}")
 
 redis_port = 6379
+
+redis_url = f"{redis_host}:{redis_port}"
 
 redis_url = redis_host + ":" + str(redis_port)
 
