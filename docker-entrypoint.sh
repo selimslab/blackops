4b7a55f22de18f62f5b318e062a1305174e0b03e
selim <selim.ozturk@afiniti.com>
2021-11-19 03:45:40 +0300

    expose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 
 # Start celery in the background
 # start with 3 workers, scale up to 10 if necessary   
-celery -A blackops.api.task worker -l info --concurrency=1 --loglevel info &
+celery flower -A blackops.taskq.tasks worker -l info --concurrency=1 --loglevel info --logfile=/var/log/celery/flower.log --pidfile=/var/run/celery/flower.pid 
 
 echo "Running CMD"
 exec "$@"
