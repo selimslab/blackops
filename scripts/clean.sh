7f62d68f37b501437906de1b9e61c2177b177ce5
selim <selim.ozturk@afiniti.com>
2021-12-22 18:09:29 +0300

    up

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,9 +1,18 @@
+mypy blackops
+
 # Remove unused imports
 autoflake --in-place -r blackops/
 
 # Auto fix flake8 warnings
 autopep8 --in-place --a --a -r blackops/
 
+isort blackops 
+
+black blackops
+
 # Run hooks on all files
-pre-commit run --all-files
+# pre-commit run --all-files
+
+
+
 
