910945ddc34f460485c8029c70aaf87dfb80eab3
selim <selim.ozturk@afiniti.com>
2022-01-22 21:33:09 +0300

    up

--- src/stgs/sliding/config.py
+++ src/stgs/sliding/config.py
@@ -21,7 +21,7 @@ class LeaderFollowerConfig(StrategyConfigBase):
 
     max_step: Decimal = Decimal(5)
 
-    quote_step_qty: Decimal = Decimal(10000)
+    quote_step_qty: Decimal = Decimal(12000)
 
     margin_bps: Decimal = Decimal(2)
 
