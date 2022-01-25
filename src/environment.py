1514ef6c61f747b375a03d35810f4eeefb502692
selim <selim.ozturk@afiniti.com>
2022-01-25 23:20:51 +0300

    UP

--- src/environment.py
+++ src/environment.py
@@ -14,7 +14,7 @@ decimal.getcontext().prec = 9
 
 class SleepSeconds(BaseModel):
 
-    clear_follower_prices: float = 0.6
+    clear_follower_prices: float = 0.5
 
     rate_limit: float = 5
 
@@ -22,16 +22,16 @@ class SleepSeconds(BaseModel):
     cancel_wait: float = 0.16  # 300/min
 
     update_balances: float = 0.72  # 90/min
-    poll_balance_update: float = update_balances / 18
+    poll_balance_update: float = update_balances / 16
 
-    refresh_open_orders: float = 0.3
+    refresh_open_orders: float = 0.4
 
-    broadcast_stats: float = 0.8
+    broadcast_stats: float = 0.9
 
-    wait_before_cancel: float = 0.12
-    wait_after_failed_order: float = 0.15
+    wait_before_cancel: float = 0.2
+    wait_after_failed_order: float = 0.2
 
-    poll_for_lock: float = 0.05
+    poll_for_lock: float = 0.07
 
 
 sleep_seconds = SleepSeconds()
