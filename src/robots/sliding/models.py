e90a8d2a735137f9a17f04e54aaf1d1fb07a9b0f
selim <selim.ozturk@afiniti.com>
2022-01-26 03:28:37 +0300

    up

--- src/robots/sliding/models.py
+++ src/robots/sliding/models.py
@@ -7,20 +7,20 @@ from src.periodic import StopwatchAPI
 
 @dataclass
 class PriceWindow:
-    buy: Decimal = Decimal(0)
     sell: Decimal = Decimal(0)
+    buy: Decimal = Decimal(0)
 
 
 @dataclass
 class MarketPrices:
-    bid: Optional[Decimal] = None
     ask: Optional[Decimal] = None
+    bid: Optional[Decimal] = None
 
 
 @dataclass
 class Signals:
-    buy: Decimal = Decimal(0)
     sell: Decimal = Decimal(0)
+    buy: Decimal = Decimal(0)
 
 
 @dataclass
