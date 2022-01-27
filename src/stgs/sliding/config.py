5fbc63710d17733211097a8546812129f3bb7323
selim <selim.ozturk@afiniti.com>
2022-01-27 12:23:34 +0300

    14,3

--- src/stgs/sliding/config.py
+++ src/stgs/sliding/config.py
@@ -32,7 +32,7 @@ class LeaderFollowerConfig(StrategyConfigBase):
     testnet = False
 
     unit_signal_bps: UnitSignalBPS = UnitSignalBPS(
-        sell=Decimal(1) * BPS, buy=Decimal(16) * BPS
+        sell=Decimal(3) * BPS, buy=Decimal(14) * BPS
     )
 
     input: LeaderFollowerInput
