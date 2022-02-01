5954c97b6efa6f149695cd5cb33093e370ccc494
selim <selim.ozturk@afiniti.com>
2022-02-01 22:14:10 +0300

    up

--- src/stgs/sliding/config.py
+++ src/stgs/sliding/config.py
@@ -24,10 +24,10 @@ class LeaderFollowerConfig(StrategyConfigBase):
     bridge: str = Field("USDT")
     bridge_exchange: ExchangeType = Field(ExchangeType.BTCTURK)
 
-    max_step: Decimal = Decimal(20)
-    sell_step: Decimal = Decimal(4)
+    max_step: Decimal = Decimal(5)
+    sell_step: Decimal = Decimal(2)
 
-    quote_step_qty: Decimal = Decimal(4000)
+    quote_step_qty: Decimal = Decimal(16000)
 
     min_sell_qty: Decimal = Decimal(400)
     min_buy_qty: Decimal = Decimal(2000)
@@ -37,7 +37,7 @@ class LeaderFollowerConfig(StrategyConfigBase):
     max_spread_bps: Decimal = Decimal(15)
 
     unit_signal_bps: UnitSignalBPS = UnitSignalBPS(
-        buy=Decimal(30) * BPS, sell=Decimal(10) * BPS
+        buy=Decimal(32) * BPS, sell=Decimal(12) * BPS
     )
 
     sell_step_per_std = max_step / Decimal(3)
