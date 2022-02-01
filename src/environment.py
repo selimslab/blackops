bf46472f3301e0c97ea37a583f75a77d7348ae95
selim <selim.ozturk@afiniti.com>
2022-02-01 23:22:36 +0300

    up

--- src/environment.py
+++ src/environment.py
@@ -27,8 +27,8 @@ class SleepSeconds(BaseModel):
 
     broadcast_stats: float = 1
 
-    wait_before_cancel: float = 0.12
-    wait_after_deliver: float = 0.12
+    wait_before_cancel: float = 0.2
+    wait_after_deliver: float = 0.2
     wait_after_failed_order: float = 0.2
 
     poll_for_lock: float = 0.05
