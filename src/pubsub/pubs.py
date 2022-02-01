a572f06d9124acb8d384328dce2b25045bb79dd0
selim <selim.ozturk@afiniti.com>
2022-02-01 16:51:23 +0300

    up

--- src/pubsub/pubs.py
+++ src/pubsub/pubs.py
@@ -138,7 +138,7 @@ class BinancePub(PublisherBase):
         self.book_stream = bn_streams.create_book_stream(self.symbol)
 
     async def run(self):
-        await asyncio.gather(self.publish_stream(), periodic(self.publish_klines, 5))
+        await asyncio.gather(self.publish_stream(), periodic(self.publish_klines, 3))
 
     def parse_book(self, book):
         try:
